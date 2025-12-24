c2f27c45928657f4ddb26b58af860ed1c5b18de6
Akhtar Hafiz P <akhfizpuli@gmail.com>
2025-12-24 15:50:07 +0700

    style: add header content and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,31 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import NavBar from './components/Navbar.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <NavBar/>
+  <div class="content">
+    <header>
+      <h1><b>Welcome to RathWeb!</b></h1>
+      <p>Aku Subtitle</p>
+    </header>
+    <div class="about">
+      <h2>About Me</h2>
+      <p></p>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
+.content {
+  margin-top: 40px;
+  margin-left: 60px;
+  margin-right: 60px;
+}
 header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
