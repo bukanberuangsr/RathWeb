ab05dd65fdad2fde89ba6440e21b443b1d5b8229
bukanberuangsr <akhfizpuli@gmail.com>
2026-05-07 11:00:30 +0700

    replace createWebHashHistory with createWebHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../pages/Home.vue'
 import MyProjects from '../pages/MyProjects.vue'
@@ -11,7 +11,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(), // pakai # (cocok untuk static hosting)
+  history: createWebHistory(),
   routes,
 })
 
