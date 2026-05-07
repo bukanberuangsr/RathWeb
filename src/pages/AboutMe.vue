41e96bf8dd1e17752f501128474b2100181581ec
bukanberuangsr <akhfizpuli@gmail.com>
2026-05-07 19:07:13 +0700

    update about page style

--- src/pages/AboutMe.vue
+++ src/pages/AboutMe.vue
@@ -1,14 +1,44 @@
 <script setup></script>
 
 <template>
-  <section>
-    <h1>About Me</h1>
-    <p>
-      Hello there! My name is Akhtar Hafiz Puliwarna, but you can call me Akhtar. I am an
-      Information Technology student at Universitas Brawijaya who is passionate about Web
-      Development, Mobile Development, and UI/UX Design.
-    </p>
-  </section>
+  <div class="content">
+    <header>
+      <h1>About Me</h1>
+    </header>
+    <section class="desc">
+      <p>
+        Hello there! My name is Akhtar Hafiz Puliwarna, but you can call me Akhtar. I am an
+        Information Technology student at Universitas Brawijaya who is passionate about Web
+        Development, Mobile Development, and UI/UX Design.
+      </p>
+    </section>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  padding: 0 60px;
+  gap: 24px;
+}
+
+header {
+  text-align: center;
+  h1 {
+    font-size: 5em;
+    font-weight: 700;
+  }
+}
+
+.desc {
+  max-width: 720px;
+  p {
+    text-align: justify;
+    font-weight: 500;
+  }
+}
+</style>
