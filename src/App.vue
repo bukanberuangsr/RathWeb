35cce001eaa7d9177d9107687c233437e8a0d67b
bukanberuangsr <akhfizpuli@gmail.com>
2026-06-23 00:42:46 +0700

    add animation when transitioning from one page to another

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,29 @@ import NavBar from './components/Navbar.vue'
 
 <template>
   <NavBar />
-  <router-view />
+  <!-- <router-view /> -->
+  <RouterView v-slot="{ Component }">
+    <Transition name="page" mode="out-in">
+      <component :is="Component" />
+    </Transition>
+  </RouterView>
 </template>
 
-<style scoped></style>
+<style scoped>
+.page-enter-active,
+.page-leave-active {
+  transition:
+    opacity 0.2s ease,
+    transform 0.2s ease;
+}
+
+.page-enter-from {
+  opacity: 0;
+  transform: translateY(8px);
+}
+
+.page-leave-to {
+  opacity: 0;
+  transform: translateY(-8px);
+}
+</style>
