24e9fe8722d07bff66ba364dffad9d20eababa13
bukanberuangsr <akhfizpuli@gmail.com>
2026-08-01 07:21:32 +0700

    add new projects and carousel height change

--- src/pages/MyProjects.vue
+++ src/pages/MyProjects.vue
@@ -9,15 +9,15 @@ const projects = ref([
 },
 {
   name: 'YASP - Yet Another Start Page',
-  image: '/images/logo-1.png',
+  image: '/images/projects/YASP.webp',
   languages: ['HTML', 'CSS', 'JS'],
   projectUrl: 'https://github.com/bukanberuangsr/YASP'
 },
 {
   name: 'Ecoin',
-  image: '/images/logo-1.png',
+  image: '/images/projects/ecoin.webp',
   languages: ['PHP', 'Vue', 'Tailwind', 'CSS'],
-  projectUrl: 'https://rathmakes.itch.io/project-dream'
+  projectUrl: 'https://github.com/TrashAlpha/Ecoin'
 }
 ])
 
@@ -28,12 +28,12 @@ const projects = ref([
     <section class="text-justify">
       <h1 class="font-bold text-7xl mb-5">Projects</h1>
        <p class="text-md my-4">This is a list of my projects</p>
-      <button class="cursor-pointer w-full flex p-2.5 bg-(--main-color) text-white text-center transition-[color,background-color,box-shadow,transform] duration-200 ease-in-out hover:bg-(--tertiary-color) hover:shadow-[0_0_20px_var(--main-color)] hover:-translate-y-0.5">
+      <button class="cursor-pointer w-full flex p-2.5 bg-(--main-color) text-white transition-[color,background-color,box-shadow,transform] duration-200 ease-in-out hover:bg-(--tertiary-color) hover:shadow-[0_0_20px_var(--main-color)] hover:-translate-y-0.5">
         Open Projects
       </button>
     </section>
-    <section class="h-100 w-130 overflow-hidden">
-      <div class="px-5 flex flex-col animate-[scroll-vertical_10s_linear_infinite] hover:[animation-play-state:paused]">
+    <section class="h-120 w-130 overflow-hidden">
+      <div class="px-5 flex flex-col animate-[scroll-vertical_15s_linear_infinite] hover:[animation-play-state:paused]">
         <div
           v-for="project in projects" :key="project.name"
           class="bg-linear-to-r from-violet-950 to-purple-900 my-3 p-3 h-120 flex flex-col items-center justify-center border-l-4 border-violet-500 hover:border-l-violet-300 transition-all hover:translate-x-2">
