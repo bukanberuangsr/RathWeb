07e7ee2d3aebb237531932520f196423a5992b39
bukanberuangsr <akhfizpuli@gmail.com>
2026-08-22 19:39:13 +0700

    I dont like purple anymore. I do not have any fiery passion at all within me.

--- src/pages/MyProjects.vue
+++ src/pages/MyProjects.vue
@@ -27,8 +27,8 @@ const projects = ref([
   <div class="min-h-[calc(100vh-80px)] flex flex-row items-center justify-center mx-auto gap-12.5">
     <section class="text-justify">
       <h1 class="font-bold text-7xl mb-5">Projects</h1>
-       <p class="text-md my-4">This is a list of my projects</p>
-      <button class="cursor-pointer w-full flex p-2.5 bg-(--main-color) text-white transition-[color,background-color,box-shadow,transform] duration-200 ease-in-out hover:bg-(--tertiary-color) hover:shadow-[0_0_20px_var(--main-color)] hover:-translate-y-0.5">
+      <p class="text-lg my-4">This is a list of my projects</p>
+      <button class="cursor-pointer w-full flex p-2.5 bg-(--main-color) text-white text-lg transition-[color,background-color,box-shadow,transform] duration-200 ease-in-out hover:bg-(from-redolor) horedadow-[0_0_20px_var(--main-color)] hover:-translate-y-0.5">
         Open Projects
       </button>
     </section>
@@ -36,14 +36,14 @@ const projects = ref([
       <div class="px-5 flex flex-col animate-[scroll-vertical_15s_linear_infinite] hover:[animation-play-state:paused]">
         <div
           v-for="project in projects" :key="project.name"
-          class="bg-linear-to-r from-violet-950 to-purple-900 my-3 p-3 h-120 flex flex-col items-center justify-center border-l-4 border-violet-500 hover:border-l-violet-300 transition-all hover:translate-x-2">
+          class="bg-linear-to-r from-red-950 to-rose-900 my-3 p-3 h-120 flex flex-col items-center justify-center border-l-4 border-rose-500 hover:border-l-rose-300 transition-all hover:translate-x-2">
           <!-- <p class="text-md font-normal">gambar</p> -->
           <img v-if="project.image" :src="project.image" :alt="project.name" loading="lazy" class="w-full h-full object-cover"/>
         </div>
         <div
           aria-hidden="true"
           v-for="project in projects" :key="project.name"
-          class="bg-linear-to-r from-violet-950 to-purple-900 my-3 p-3 h-120 flex flex-col items-center justify-center border-l-4 border-violet-500 hover:border-l-violet-300 transition-all hover:translate-x-2">
+          class="bg-linear-to-r from-red-950 to-rose-900 my-3 p-3 h-120 flex flex-col items-center justify-center border-l-4 border-rose-500 hover:border-l-rose-300 transition-all hover:translate-x-2">
           <!-- <p class="text-md font-normal">gambar</p> -->
           <img v-if="project.image" :src="project.image" :alt="project.name" loading="lazy" class="w-full h-full object-cover"/>
         </div>
